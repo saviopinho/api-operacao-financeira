3be4523e4aced2996078b1ebdc3b30f3e91d813e
SAVIO PINHO NUNES <savio.pinho@hotmail.com>
2022-11-11 12:17:54 -0300

    teste

--- src/controllers/LoginController.ts
+++ src/controllers/LoginController.ts
@@ -3,7 +3,6 @@ import { BadRequestError, UnauthorizeError } from '../helper/ApiError';
 import { peopleRepo } from '../data/repositories/peopleRepository';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
-import Utils from '../helper/utils';
 
 export const login = async (
     req: Request,
@@ -11,27 +10,22 @@ export const login = async (
     next: NextFunction
 ) => {
     const { document, password } = req.body;
-    const formatedDocument = Utils.onlyNumbers(document);
 
     if (!(document && password)) {
         throw new BadRequestError('All input is required');
     }
 
     const foundPeople = await peopleRepo.findOneBy({
-        document: formatedDocument,
+        document,
     });
 
     if (
         foundPeople &&
         (await bcrypt.compare(password, foundPeople!.password))
     ) {
-        const token = jwt.sign(
-            { formatedDocument },
-            process.env.API_KEY ?? '',
-            {
-                expiresIn: '24h',
-            }
-        );
+        const token = jwt.sign({ document }, process.env.API_KEY ?? '', {
+            expiresIn: '24h',
+        });
 
         res.status(200).json({ token: `Bearer ${token}` });
     } else {
---
+++ src/data/migrations/1668179712064-default.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class default1668179712064 implements MigrationInterface {
+    name = 'default1668179712064'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "cards" ("id" text NOT NULL, "type" text NOT NULL, "number" text NOT NULL, "cvv" text NOT NULL, "document" text NOT NULL, "accountId" text NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, CONSTRAINT "PK_5f3269634705fdff4a9935860fc" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "accounts" ("id" text NOT NULL, "branch" text NOT NULL, "account" text NOT NULL, "document" text NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, CONSTRAINT "PK_5a7a02c20412299d198e097a8fe" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "transactions" ("id" text NOT NULL, "value" numeric(12,2) NOT NULL, "description" text NOT NULL, "accountId" text NOT NULL, "reversedAt" TIMESTAMP WITH TIME ZONE, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, CONSTRAINT "PK_a219afd8dd77ed80f5a862f1db9" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE TABLE "people" ("id" text NOT NULL, "document" text NOT NULL, "name" text NOT NULL, "password" text NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, "updatedAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(6) with time zone, CONSTRAINT "UQ_997a421a0bac0513909d23a1c2a" UNIQUE ("document"), CONSTRAINT "PK_aa866e71353ee94c6cc51059c5b" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "people"`);
+        await queryRunner.query(`DROP TABLE "transactions"`);
+        await queryRunner.query(`DROP TABLE "accounts"`);
+        await queryRunner.query(`DROP TABLE "cards"`);
+    }
+
+}
